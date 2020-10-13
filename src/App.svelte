416f8bb53ba64c4efa8d734d1efd8403fe4a0b52
b1mind <brent@1mindsolutions.com>
2020-10-13 14:03:07 -0600

    🧪 testing and relative paths

--- src/App.svelte
+++ src/App.svelte
@@ -1,49 +1,65 @@
 <script>
-  export let name
+  export let userName
+  export let age
+
   let val = 420
 </script>
 
 <main>
   <h1>This is a {val} test smoke it</h1>
 
+  {#if userName}
+    <h2>Hello {userName} you are {age} years old</h2>
+  {:else}
+    <h2>Hello World</h2>
+  {/if}
+
   <aside>
-    <h2>aside bar</h2>
+    <p>see you on the other side</p>
   </aside>
 
-  <!-- prettier-ignore -->
-  <template lang="pug">
-    +if ('name')
+  <!-- pug still has some vscode issues but compiles fine -->
+  <!-- prettier-ignore  
+    <template lang="pug">
+    +if ("name")
       h1 Hello {name}
       +else 
         h1 Hello enter name
     p This is a test of the pug system
-  </template>
+  </template> 
+  -->
 </main>
 
 <style lang="scss">
-  ::placeholder {
-    color: red;
-  }
+  // todo add some style
 
   main {
-    text-align: center;
-    padding: 1em;
-    max-width: 240px;
     margin: 0 auto;
-    h1 {
+    padding: 1em;
+    text-align: center;
+    max-width: 240px;
+
+    h2 {
       color: skyblue;
+      font-weight: 700;
+    }
+
+    @media (min-width: 640px) {
+      h2 {
+        color: greenyellow;
+      }
+      max-width: none;
     }
   }
 
   h1 {
+    color: red;
     text-transform: uppercase;
-    font-size: 4em;
     font-weight: 100;
-  }
 
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
+    @media (min-width: 640px) {
+      color: white;
+      font-size: 4rem;
     }
   }
 </style>
